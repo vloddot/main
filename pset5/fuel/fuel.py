dcc23e55861c51b80924ea585b2c1b0b1cc84635
vloddot <tamoraa08@gmail.com>
2022-05-20 11:34:44 +0200

    hello?

--- pset5/fuel/fuel.py
+++ pset5/fuel/fuel.py
@@ -1,9 +1,5 @@
 def main():
-<<<<<<< HEAD
     fraction = input('Fraction: ').strip()
-=======
-    fraction = input('Fraction: ')
->>>>>>> 3789f49be75645444e2f8be47c551d8abac42471
     percentage = convert(fraction)
     print(gauge(percentage))
 
@@ -12,40 +8,25 @@ def convert(fraction: str) -> int:
     x, y = fraction.split('/')
     x, y = float(x), float(y)
 
-<<<<<<< HEAD
     if x % 1 != 0 or y % 1 != 0:
-=======
-    if x > y or x % 1 != 0 or y % 1 != 0:
->>>>>>> 3789f49be75645444e2f8be47c551d8abac42471
         raise ValueError
 
     if y == 0:
         raise ZeroDivisionError
 
-<<<<<<< HEAD
     if x > y:
         raise ValueError
 
     result = (x / y) * 100
-=======
-    result = float(x / y * 100)
->>>>>>> 3789f49be75645444e2f8be47c551d8abac42471
 
     if result > 100:
         raise ValueError
 
-<<<<<<< HEAD
     return int(result)
 
 
 def gauge(percentage: int) -> str:
 
-=======
-    return int(round(result))
-
-
-def gauge(percentage: float) -> str:
->>>>>>> 3789f49be75645444e2f8be47c551d8abac42471
     if percentage >= 99:
         return 'F'
 
--- pset5/fuel/test_fuel.py
+++ pset5/fuel/test_fuel.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import pytest
-=======
->>>>>>> 3789f49be75645444e2f8be47c551d8abac42471
 from fuel import convert, gauge
 
 
 def test_convert():
-<<<<<<< HEAD
     with pytest.raises(ValueError) as exc:
         x = convert('2/1')
 
@@ -28,17 +24,3 @@ def test_gauge():
     assert gauge(99) == 'F'
     assert gauge(100) == 'F'
     assert gauge(50) == '50%'
-=======
-    assert convert('3/4') == 75
-    assert convert('0/4') == 0
-    assert convert('1/2') == 50
-    assert convert('4/5') == 80
-
-
-def test_gauge():
-    assert gauge(100) == 'F'
-    assert gauge(0) == 'E'
-    assert gauge(67) == '67%'
-    assert gauge(20) == '20%'
-    assert gauge(0.5) == 'E'
->>>>>>> 3789f49be75645444e2f8be47c551d8abac42471
